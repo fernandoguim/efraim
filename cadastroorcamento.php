6147e48681cc59a42c16a8b295f46bedfe87103c
fernandoguim <39016558+fernandoguim@users.noreply.github.com>
2022-01-03 23:19:17 -0300

    https://github.com/fernandoguim/efraim

--- cadastroorcamento.php
+++ cadastroorcamento.php
@@ -1,7 +1,4 @@
 <?PHP include 'painel.php';
-require 'bdLogin/select.class.php';
-
-$d = new selectoi();
  
 
 ?>
@@ -20,14 +17,13 @@ $d = new selectoi();
                 </li>
                 </ul>
                 <div class="page-toolbar">
-                <div id="dashboard-report-range" class="pull-right tooltips btn btn-sm" data-container="body" data-placement="bottom" data-original-title="Change dashboard date range">
-                <i class="icon-calendar"></i>&nbsp;
-                <span class="thin uppercase hidden-xs"></span>&nbsp;
-                <i class="fa fa-angle-down"></i>
-                </div>
+               
                 </div>
                 </div>
                 <br>
+                
+                 <?php if($_REQUEST['id'] != "2") {?>
+                
                 <div class="portlet box green">
                 <div class="portlet-title">
                 <div class="caption">
@@ -37,7 +33,7 @@ $d = new selectoi();
                 </div>
                 </div>
                 <div class="portlet-body form">
-                <form class="form-horizontal" role="form" method="post" name="registro" id="registro">
+                <form class="form-horizontal" role="form" method="post" name="registro" id="registro" action="cadastroorcamento">
                 <div class="form-body">
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>CLIENTE:</strong></label>
@@ -49,8 +45,10 @@ $d = new selectoi();
                 $sqlcliente= $d->buscarclientes();//envia para a function
                 foreach ($sqlcliente as $cliente) :?>
                 <option value="<?php echo strtoupper($cliente['cliente']) ?>"><?php echo strtoupper($cliente['cliente']);?></option>
+                
                 <?php endforeach; ?> 
                 </select>
+                <input type="hidden" class="form-control" name="id" id="id" value = "2">
                 </div>
                 </div>
                 <div class="form-group">
@@ -70,7 +68,149 @@ $d = new selectoi();
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>DEMANDA:</strong></label>
                 <div class="col-md-3">
-                <input type="text" class="form-control" placeholder="Descrição do Demanda" name="servico" id="servico" required> 
+                <input type="text" class="form-control" placeholder="Descrição do Demanda" name="demanda" id="demanda" required> 
+                </div>
+                </div>
+                 <div class="form-group">
+                <label class="col-md-2 control-label"><strong>SERVIÇO:</strong></label>
+                <div class="col-md-3">
+                 <textarea name="servico" id="servico" cols="88" rows="5" required></textarea>  
+                 </div>
+                </div>
+                 <div class="form-group">
+                <label class="col-md-2 control-label"><strong>ÁREA DE PROJEÇÃO:</strong></label>
+                <div class="col-md-4">
+                 <input type="text" class="form-control" placeholder="ÁREA DE PROJEÇÃO" name="areaprojecao" id="areaprojecao" required> 
+                 </div>
+                </div>
+                
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>TELHA:</strong></label>
+                <div class="col-md-4">
+                 <textarea name="telha" id="telha" cols="88" rows="5" required></textarea>  
+                 </div>
+                </div>
+                
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>CALHAS:</strong></label>
+                <div class="col-md-4">
+               <textarea name="calhas" id="calhas" cols="88" rows="5" required></textarea>  
+                </div>
+                </div>
+                
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>PINTURAS:</strong></label>
+                <div class="col-md-4">
+                <textarea name="pinturas" id="pinturas" cols="88" rows="5" required></textarea>  
+                </div>
+                </div>
+                
+                <hr>
+                 
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>RESPONSAVEL:</strong></label>
+                <div class="col-md-4">
+                <input type="hidden" class="form-control" name="responsavel" id="responsavel" value="<?php echo $_SESSION['Nome']?>" > <input type="text" class="form-control" name="vendedorx" id="vendedorx" value="<?php echo $_SESSION['Nome']?>" disabled>
+                </div>
+                </div>
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>OBSERVAÇÃO:</strong></label>
+                <div class="col-md-5">
+                <textarea name="obs" id="obs" cols="88" rows="5" value="" required><?php echo $REQUEST['obs']?></textarea>
+                </div>
+                </div>
+                 
+               
+                </div>
+    
+                <div class="form-actions right1">
+                <button type="submit" class="btn default">Limpar</button>
+                <button type="submit" class="btn green" name="regvenda" id= "regvenda" value ="regvenda">Registrar</button>
+                </div>
+                </form>
+                </div>
+                </div>
+                 <?php } ?> 
+                 
+                 <?php 
+                
+                $l = new selectoi();
+                if ($_POST['regvenda']!="") {
+                $cliente= addslashes($_POST['cliente']);
+                $orcamento= addslashes($_POST['orcamento']);
+                $proposta= addslashes($_POST['proposta']);
+                $demanda= addslashes($_POST['demanda']);
+                $servico = addslashes($_POST['servico']);
+                $areaprojecao= addslashes($_POST['areaprojecao']);
+                $telha= addslashes($_POST['telha']);
+                $calhas= addslashes($_POST['calhas']);
+                $pinturas = addslashes($_POST['pinturas']);
+                $responsavelr = addslashes($_POST['responsavel']);
+                $obs = addslashes($_POST['obs']);
+                $l->registroorcamento($cliente,$orcamento,$proposta,$demanda,$servico,$areaprojecao,$telha,$calhas,$pinturas,$responsavelr,$obs);// está enviando para a classe 
+                
+                $msgg= "<div class='alert alert-success'>Cadastro realizado com sucesso!!!</div>";
+                ?>
+                
+                <script>
+                window.setTimeout(function() {
+                 },  );
+                </script>
+                <?php // depois de 3 segundo redireciona
+                echo $msgg; //posta a ensagem
+                }
+                ?>
+               
+                
+                   <?php if($_REQUEST['id'] == "2"){?>
+                
+                <div class="portlet box green">
+                <div class="portlet-title">
+                <div class="caption">
+                <i class="fa fa-user-plus"></i> Cadastro de Orçamento </div>
+                <?php  
+                 
+                $d = new selectoi();
+                $matricula = $_SESSION['Nome'];
+                $cliente = $_REQUEST['cliente'];
+                
+                $sqlultimoorcamento = $d->ultimoorcamento($matricula,$cliente);//envia para a function
+                foreach ($sqlultimoorcamento as $orcamento) :?>
+                <div class="tools">
+                  <?php if($orcamento['registroid'] != "") {?><a href="imprimir?registroid=<?php echo $orcamento['registroid'] ?>" ><img src="Logo-PDF-PNG.png" alt="mspantanal" width="30" height="30"></a><?php } ?> 
+                </div>
+                </div>
+                <div class="portlet-body form">
+                
+                <form class="form-horizontal" role="form" method="post" name="registro2" id="registro2" action="cadastroorcamento">
+                <div class="form-body">
+                                    
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>CLIENTE:</strong></label>
+                <div class="col-md-3">                                                                                 
+                <input type="text" class="form-control" placeholder="Cliente" name="cliente" id="cliente" value = "<?php echo $orcamento['cliente'] ?>" > 
+                <input type="hidden" class="form-control" name="id" id="id" value = "2">
+                <input type="hidden" class="form-control" placeholder="proposta" name="registroid" id="registroid" value = "<?php echo $orcamento['registroid'] ?>">
+                </div>
+                </div>
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>ORÇAMENTO</strong></label>
+                <div class="col-md-5">
+                <input type="text" class="form-control" placeholder="Descrição orçamento" name="orcamento" id="orcamento" value = "<?php echo $orcamento['orcamento'] ?>"> 
+                </div>
+                 
+                </div>
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>PROPOSTA:</strong></label>
+                <div class="col-md-3">
+                <input type="text" class="form-control" placeholder="proposta" name="proposta" id="proposta" value = "<?php echo $orcamento['proposta'] ?>">
+                </div>
+                </div>
+                <hr>
+                <div class="form-group">
+                <label class="col-md-2 control-label"><strong>DEMANDA:</strong></label>
+                <div class="col-md-3">
+                <input type="text" class="form-control" placeholder="Descrição do Demanda" name="demanda" id="demanda" required> 
                 </div>
                 </div>
                  <div class="form-group">
@@ -89,21 +229,21 @@ $d = new selectoi();
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>TELHA:</strong></label>
                 <div class="col-md-4">
-                 <textarea name="telha" id="telha" cols="88" rows="5"></textarea>  
+                 <textarea name="telha" id="telha" cols="88" rows="5" required></textarea>  
                  </div>
                 </div>
                 
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>CALHAS:</strong></label>
                 <div class="col-md-4">
-               <textarea name="calhas" id="calhas" cols="88" rows="5"></textarea>  
+               <textarea name="calhas" id="calhas" cols="88" rows="5" required></textarea>  
                 </div>
                 </div>
                 
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>PINTURAS:</strong></label>
                 <div class="col-md-4">
-                <textarea name="pinturas" id="pinturas" cols="88" rows="5"></textarea>  
+                <textarea name="pinturas" id="pinturas" cols="88" rows="5" required></textarea>  
                 </div>
                 </div>
                 
@@ -112,64 +252,62 @@ $d = new selectoi();
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>RESPONSAVEL:</strong></label>
                 <div class="col-md-4">
-                <input type="hidden" class="form-control" name="vendedor" id="vendedor" value="<?php echo $_SESSION['Nome']?>" > <input type="text" class="form-control" name="vendedorx" id="vendedorx" value="<?php echo $_SESSION['Nome']?>" disabled>
+                <input type="hidden" class="form-control" name="responsavel" id="responsavel" value="<?php echo $_SESSION['Nome']?>" > <input type="text" class="form-control" name="vendedorx" id="vendedorx" value="<?php echo $_SESSION['Nome']?>" disabled>
                 </div>
                 </div>
                 <div class="form-group">
                 <label class="col-md-2 control-label"><strong>OBSERVAÇÃO:</strong></label>
                 <div class="col-md-5">
-                <textarea name="obs" id="obs" cols="88" rows="5" value=""><?php echo $REQUEST['obs']?></textarea>
+                <textarea name="obs" id="obs" cols="88" rows="5" value="" required> </textarea>
                 </div>
                 </div>
-                 
-                <?php 
-                 if ($_SESSION['parceiro'] == "" ){
-	
-                 echo  "<script>alert('Você deve logar no Sistema para prosseguir com o registro, Caso contrario, registro sem vendedor!');</script>"; 
-                  	
-                    }
-                $l = new selectoi();
-                if ($_POST['cliente']!="") {
-                $cliente= addslashes($_POST['cliente']);
-                $cpf= addslashes($_POST['cpf']);
-                $contato1= addslashes($_POST['contato1']);
-                $contato2= addslashes($_POST['contato2']);
-                $rg= addslashes($_POST['rg']);
-                $vendedor= addslashes($_POST['vendedor']);
-                $email= addslashes($_POST['email']);
-                $logradouro = addslashes($_POST['endereco']);
-                $numfachada = addslashes($_POST['NUM']);
-                $cep = addslashes($_POST['CEP']);
-                $bairro = addslashes($_POST['BAIRRO']);
-                $cidade = addslashes($_POST['CIDADE']);
-                $obs = addslashes($_POST['obs']);
-                
-                #$l->registrocliente($cliente,$cpf,$contato1,$contato2,$rg,$vendedor,$email,$logradouro,$numfachada,$cep,$bairro,$cidade,$obs);// está enviando para a classe 
-                
-                
-                $msgg= "<div class='alert alert-success'>Cadastro realizado com sucesso!!!</div>";
-                ?>
-                
-                <script>
-                window.setTimeout(function() {
-                   if(confirm("Deseja cadastrar proxima demanda do orçamento?")){
-                   window.location = 'cadastroorcamento2.php?cliente=<?php echo $_REQUEST['cliente']?>';
-                  }
-                }, 2000);
-                </script>
-                <?php // depois de 3 segundo redireciona
-                echo $msgg; //posta a ensagem
-                }
-                ?>
+             
+               
                 
                 </div>
     
                 <div class="form-actions right1">
                 <button type="submit" class="btn default">Limpar</button>
-                <button type="submit" class="btn green" name="regvenda" id= "regvenda">Registrar</button>
+                <button type="submit" class="btn green" name="regvenda2" id= "regvenda2" value= "regvenda2">Registrar</button>
                 </div>
                 </form>
+                 <?php endforeach; ?> 
                 </div>
                 </div>
+                 <?php } ?> 
+                
+                <?php 
+                
+                $l = new selectoi();
+                if ($_POST['regvenda2']!="") {
+                $cliente= addslashes($_POST['cliente']);
+                $orcamento= addslashes($_POST['orcamento']);
+                $proposta= addslashes($_POST['proposta']);
+                $demanda= addslashes($_POST['demanda']);
+                $servico = addslashes($_POST['servico']);
+                $areaprojecao= addslashes($_POST['areaprojecao']);
+                $telha= addslashes($_POST['telha']);
+                $calhas= addslashes($_POST['calhas']);
+                $pinturas = addslashes($_POST['pinturas']);
+                $responsavelr = addslashes($_POST['responsavel']);
+                $registroid = addslashes($_POST['registroid']);
+                $obs = addslashes($_POST['obs']);
+                
+                $l->registroorcamento2($cliente,$orcamento,$proposta,$demanda,$servico,$areaprojecao,$telha,$calhas,$pinturas,$responsavelr,$registroid,$obs);// está enviando para a classe 
+                
+                $msgg= "<div class='alert alert-success'>Cadastro realizado com sucesso!!!</div>";
+                ?>
+                
+                
+                
+                <script>
+                window.setTimeout(function() {
+              
+                }, );
+                </script>
+                <?php // depois de 3 segundo redireciona
+                echo $msgg; //posta a ensagem
+                }
+                ?>
                    
                 <?PHP include 'fechacodigo.php' ?> 
